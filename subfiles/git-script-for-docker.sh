f9e119878c320d4ffcbbb7a69d7a876f40789473
goya5858 <hokuzaki1203@gmail.com>
2021-12-05 01:52:47 +0900

    add sample

--- subfiles/git-script-for-docker.sh
+++ subfiles/git-script-for-docker.sh
@@ -8,5 +8,9 @@ chmod 400 /root/.ssh/github_ssh
 # Gitのデフォルトブランチを main に変更
 git config --global init.defaultBranch main
 
+# AWS CLI用のクレデンシャル情報をコンテナの環境変数に設定
+AWS_ACCESS_KEY_ID=$(cat ~/.ssh/env.json | jq ".secrets.AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY_ID=$(cat ~/.ssh/env.json | jq ".secrets.AWS_SECRET_ACCESS_KEY_ID")
+
 # コンテナを起動し続ける
 tail -f /dev/null
