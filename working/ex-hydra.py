31a7fba176d9695e67da1a9790ba3c212b0c1f42
goya5858 <hokuzaki1203@gmail.com>
2021-12-09 21:33:25 +0900

    微調整

--- working/ex-hydra.py
+++
@@ -1,9 +0,0 @@
-import hydra
-from omegaconf import dictconfig
-
-@hydra.main(config_name="config.yaml", config_path=".")
-def main(cfg: dictconfig.DictConfig) ->None:
-    print(cfg)
-
-if __name__ == "__main__":
-    main()
